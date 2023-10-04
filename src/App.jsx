ad579eb788c136efcb9e350e335abd4ce2057e80
alex.herrera <alex.guialexander@gmail.com>
2023-10-04 17:02:24 -0500

    feat: ajuste de routes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routing } from './router/Routing.jsx';
+
+function App() {
+  return (
+    <>
+      <div className="layout">
+        {/*Cargando toda la configuración de rutas*/}
+        <Routing />
+      </div>
+    </>
+  );
+}
+
+export default App;
