4f373cf02ac713c3665760a0c29eaec86dc3b54c
alex.herrera <alex.guialexander@gmail.com>
2023-10-05 09:00:32 -0500

    feat:navegación Login anf register

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <header class="content__header content__header--public">
+      <h1 class="content__title">Login</h1>
+      <div class="content__posts"></div>
+    </header>
+  );
 };
--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <header class="content__header content__header--public">
+        <h1 class="content__title">Registro</h1>
+        <div class="content__posts"></div>
+      </header>
+    </>
+  );
 };
